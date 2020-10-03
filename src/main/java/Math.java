5bb512e9b5de05fc3efe0ffb0c3ae2bef3c26ff5
Bipin Ayetra <bipin.ayetra1@t-mobile.com>
2020-10-03 00:33:44 -0700

    Initial commit

---
+++ src/main/java/Math.java
@@ -0,0 +1,8 @@
+public class Math {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+    public int add(int A, int B){
+        return A+B;
+    }
+}
